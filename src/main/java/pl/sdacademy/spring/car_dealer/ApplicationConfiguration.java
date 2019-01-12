ab89d87f351707e64f4c97b00813bf868e365e2a
Magdalena Gąsior <m.gasior09@gmail.com>
2019-01-12 13:56:18 +0100

    ApplicationConfiguration first annotations

--- src/main/java/pl/sdacademy/spring/car_dealer/ApplicationConfiguration.java
+++ src/main/java/pl/sdacademy/spring/car_dealer/ApplicationConfiguration.java
@@ -1,31 +1,45 @@
 package pl.sdacademy.spring.car_dealer;
 
+import org.springframework.beans.factory.annotation.Autowire;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import pl.sdacademy.spring.car_dealer.controller.CarDataController;
 import pl.sdacademy.spring.car_dealer.controller.SellingController;
 import pl.sdacademy.spring.car_dealer.repository.*;
-import pl.sdacademy.spring.car_dealer.service.CarDataService;
 import pl.sdacademy.spring.car_dealer.service.DefaultCarDataService;
 import pl.sdacademy.spring.car_dealer.service.DefaultSellingService;
-import pl.sdacademy.spring.car_dealer.service.SellingService;
 
 @Configuration
+@PropertySource("application.properties")
 public class ApplicationConfiguration {
 
-    @Bean("hardDriveCustomerRepository")
+    @Value("${repository.customer.hardDrive.fileLocation}")
+    private String customerRepoLocation;
+    @Value("${repository.purchase.hardDrive.fileLocation}")
+    private String purchaseRepoLocation;
+
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean(name = "hardDriveCustomerRepository", initMethod = "initialize", destroyMethod = "cleanUp")
     public HardDriveCustomerRepository hardDriveCustomerRepository() {
-        return new HardDriveCustomerRepository("customers.ser");
+        return new HardDriveCustomerRepository(customerRepoLocation);
     }
 
-    @Bean
-    public HardDriveVehicleRepository HardDriveVehicleRepository() {
-        return new HardDriveVehicleRepository("vehicles.ser");
+    @Bean(name = "hardDriveVehicleRepository", initMethod = "initialize", destroyMethod = "cleanUp")
+    public HardDriveVehicleRepository hardDriveVehicleRepository(@Value("${repository.vehicle.hardDrive.fileLocation}") String repoLocation) {
+        return new HardDriveVehicleRepository(repoLocation);
     }
 
-    @Bean
+    @Bean(name = "hardDrivePurchaseRepository", initMethod = "initialize", destroyMethod = "cleanUp")
     public HardDrivePurchaseRepository hardDrivePurchaseRepository() {
-        return new HardDrivePurchaseRepository("purchases.ser");
+        return new HardDrivePurchaseRepository(purchaseRepoLocation);
     }
 
     @Bean
@@ -33,30 +47,24 @@ public class ApplicationConfiguration {
         return new DefaultCarDataService(vehicleRepository);
     }
 
-    @Bean
+    @Bean(name = "defaultSellingService")
     public DefaultSellingService defaultSellingService(VehicleRepository vehicleRepository, CustomerRepository customerRepository, PurchaseRepository purchaseRepository) {
         return new DefaultSellingService(vehicleRepository, customerRepository, purchaseRepository);
     }
 
-    @Bean
-    public CarDataController carDataController(CarDataService carDataService) {
-        CarDataController carDataController = new CarDataController();
-        carDataController.setCarDataService(carDataService);
-        return carDataController;
+    @Bean(name = "carDataController", autowire = Autowire.BY_TYPE)
+    public CarDataController carDataController() {
+        return new CarDataController();
+
     }
 
-    @Bean
-    public SellingController sellingController(SellingService sellingService) {
-        SellingController sellingController = new SellingController();
-        sellingController.setSellingService(sellingService);
-        return sellingController;
+    @Bean(name = "sellingController", autowire = Autowire.BY_TYPE)
+    public SellingController sellingController() {
+        return new SellingController();
     }
 
-    @Bean
-    public Application application(SellingController sellingController, CarDataController carDataController) {
-        Application application = new Application();
-        application.setCarDataController(carDataController);
-        application.setSellingController(sellingController);
-        return application;
+    @Bean(name = "application", autowire = Autowire.BY_TYPE)
+    public Application application() {
+        return new Application();
     }
 }
