d0fcc530857581642aa28eab194c3ad4c8dc4627
Magdalena Gąsior <m.gasior09@gmail.com>
2019-01-20 15:41:39 +0100

    Roles used in access control

--- src/main/java/pl/sdacademy/spring/car_dealer/SecurityConfiguration.java
+++ src/main/java/pl/sdacademy/spring/car_dealer/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package pl.sdacademy.spring.car_dealer;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -14,17 +15,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     public UserDetailsService inMemoryUserDetailsManager() {
         InMemoryUserDetailsManager userDetailsManager = new InMemoryUserDetailsManager();
-        userDetailsManager.createUser(User.withDefaultPasswordEncoder().username("Ecik").password("ecik123").roles("user").build());
-        userDetailsManager.createUser(User.withDefaultPasswordEncoder().username("Adelajda").password("lampka").roles("user").build());
+        userDetailsManager.createUser(User.withDefaultPasswordEncoder().username("Ecik").password("ecik123").roles("user", "add").build());
+        userDetailsManager.createUser(User.withDefaultPasswordEncoder().username("Adelajda").password("lampka").roles("user", "sell", "purchases").build());
         return userDetailsManager;
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/vehicles/add").hasRole("add")
+                .antMatchers("/vehicles/**/sell").hasRole("sell")
+                .antMatchers(HttpMethod.POST,"/vehicles").hasRole("sell")
+                .antMatchers("/purchases/**").hasRole("purchases")
+                .anyRequest().permitAll()
                 .and().formLogin().defaultSuccessUrl("/vehicles")
-                .and().logout().logoutSuccessUrl("/login")
+                .and().logout().logoutSuccessUrl("/vehicles")
                 .and().httpBasic();
     }
 }
